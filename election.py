cbbccd84d7a8d12d94675f9d10e049d5c768609b
Egzon Syka <egzon.syka@students.unibe.ch>
2016-05-01 15:35:00 +0200

    Started working in election implementation

--- election.py
+++ election.py
@@ -6,21 +6,35 @@ from kazoo.exceptions import KazooException
 
 class Election:
 
-    def __init__(self, zk, path, func,args):
-        self.election_path = path
+   def __init__(self, zk, election_path, self_path):
+        self.election_path = election_path
         self.zk = zk
-		self.is_leader = False
-        if not (inspect.isfunction(func)) and not(inspect.ismethod(func)):
-            logging.debug("not a function "+str(func))
-            raise SystemError
-		#TO COMPLETE
+	self.is_leader = False
+	self.child_id = self_path.split("/")[2]
+	self.path = self_path
+	self.leader_path = None
+    
 		 
     def is_leading(self):
 		#TO COMPLETE
 		
 	#perform a vote..	
     def ballot(self,children):
-		#TO COMPLETE
+	next_master = min(children)
+	master_path = self.election_path + "master_current/"
+	self.zk.ensure_path(master_path)
+	self.leader_path = master_path + next_master
+	if(self.child_id == next_master) :
+		self.zk.create(self.leader_path, ephemeral=True) 
+		print("I'm the leader now")
+		self.is_leader = True
+		return True
+	else:
+		print ("Master is = %s " %(self.leader_path)) 
+		self.zk.exists(self.leader_path, self.on_node_delete) 
+		self.is_leader = False
+		return False
+
                     
 if __name__ == '__main__':
     zkhost = "127.0.0.1:2181" #default ZK host
