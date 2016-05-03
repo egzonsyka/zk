e0f7c36d873e5ab7ae30a7e906e439aaaf565028
Labinot Jakupi <labinot.jakupi@students.unibe.ch>
2016-05-03 10:04:00 +0200

    Started working on client implementation

--- client.py
+++ client.py
@@ -10,9 +10,24 @@ class Client:
 
     def __init__(self,zk):
         self.zk = zk
+
+    def compute_task(self):
+	task_id = random.randint(100, 99999)
+	task = TASKS_PATH + str(task_id)
+	task_path = self.zk.create(task, ephemeral = True)
+	task_data = str(task_id)
+	#Setting data
+	zk.set(task, task_data)
+	data = DATA_PATH + str(task_id)
+	data_path = self.zk.create(data, ephemeral = True)
+	data_value = str(random.randint(1, 100))
+	zk.set(data_path, data_value)
+	print("New task with id  = %s and data = %s" %(task_data, data_value))
+	return task_path
 	
     def submit_task(self):
-		#TO COMPLETE
+        new_task_path = self.compute_task()
+	watcher = zk.DataWatch(new_task_path, self.task_completed)
 		
 	#REACT to changes on the submitted task..				   
     def task_completed(self,data,stat):
