d2fbc31cd563c0dc9c178b0c915fb7ec90495871
Labinot Jakupi <labinot.jakupi@students.unibe.ch>
2016-05-05 13:08:00 +0200

    Worker constructor and change of assignment implemented. Need to show somehow the results

--- worker.py
+++ worker.py
@@ -8,14 +8,29 @@ from utils import WORKERS_PATH
 
 class Worker:
 
-    def __init__(self,zk):
+   def __init__(self,zk):
         self.zk = zk
-        self.uuid = uuid.uuid4()
-		#complete by creating and watching proper nodes...		 
+        #1.choose a random id
+	self.uuid = uuid.uuid4()
+	self.path = WORKERS_PATH + str(self.uuid)
+        #2.create znode
+	self.worker_id = zk.create(self.path, ephemeral=True)
+	zk.set(self.worker_id, "non")
+	print("Worker %s  created!" %(self.worker_id))
+	#3.watch znode
+	zk.DataWatch(self.path, self.assignment_change)   		 
 
-    #do something upon the change on assignment    
-    def assignment_change(self,atask,stat):
-		#to complete
+     # do something upon the change on assignment
+    def assignment_change(self, atask, stat):
+	if atask and not atask == "non" :
+		#4.5. get task id uppon assignment in workers, get task data in data/yyy
+		data_path = DATA_PATH + atask
+		if self.zk.exists(data_path) :
+			data = self.zk.get(data_path)
+			#6. execute task with data
+			result = utils.task(data)
+			task_path = TASKS_PATH + atask
+			task_val = atask + "=" + str(result)
 
 if __name__ == '__main__':
     zk = utils.init()    
